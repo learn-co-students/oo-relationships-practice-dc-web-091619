dac80dc3f044d4cfc4491594dcd0007e6fd6e808
Will <comfortwb21@gmail.com>
2019-09-20 18:46:13 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,32 @@ def reload
   load 'config/environment.rb'
 end
 
+#BAKERY TESTS
+#initialize(name, desserts)
+
+pete= Bakery.new("Pete's")
+carlos = Bakery.new("Carlos")
+
+
+
+#DESSERTS TESTS
+# #initialize (name, bakery)
+ velvet_cake= Dessert.new("red velvet cake", pete)
+lava_cake = Dessert.new("lava cake", carlos)
+cookies = Dessert.new("cookies", pete)
+
+#INGREDIENTS TESTS
+#(name, dessert)
+flour = Ingredient.new("flour", velvet_cake, 10)
+chocolate = Ingredient.new("chocolate", lava_cake, 25)
+chips = Ingredient.new("chocolate chips", cookies, 15)
+c_flour = Ingredient.new("cake flour", lava_cake, 20)
+
+
 binding.pry
 0
+
+
+
+
+
