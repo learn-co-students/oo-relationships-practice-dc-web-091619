cea11b0f92787ddb738d2c66ee57fd051f8ca74b
kh45 <khaledhassan45@gmail.com>
2019-09-23 15:19:22 -0400

    Done.

--- app/models/rides.rb
+++ app/models/rides.rb
@@ -5,7 +5,7 @@ class Ride
     @@all = []
 
     def initialize(distance, passenger, driver)
-        @distance = distance
+        @distance = distance.to_f
         @passenger = passenger
         @driver = driver
         @@all << self
