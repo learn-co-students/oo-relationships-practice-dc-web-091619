f3a9228aa26ff1afa5995f103ce2793ce20687a5
nihaprezz <sergio13prez@gmail.com>
2019-09-23 08:56:10 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,8 @@ def reload
   load 'config/environment.rb'
 end
 
+#### AIR BNB TESTS ###
+
 #Listing 
 chicago = Listing.new("Appartment", "Chicago")
 seattle = Listing.new("Studio", "Seattle")
@@ -43,6 +45,43 @@ harry.listings   #WORKS
 johnny.trip_count   #WORKS
 Guest.pro_traveller   #WORKS 
 Guest.find_all_by_name("Johnny") #WORKS
+
+### LYFT TESTS ###
+
+## Passengers
+sergio = Passenger.new("Sergio")
+billy = Passenger.new("Billy")
+drake = Passenger.new("Drake")
+
+## Drivers
+harry = Driver.new("Harry")
+henry = Driver.new("Henry")
+franklin = Driver.new("Franklin")
+
+## Rides
+ride1 = Ride.new(2.1, sergio, harry)
+ride2 = Ride.new(4.1, sergio, franklin)
+ride3 = Ride.new(2.1, billy, harry)
+ride4 = Ride.new(5.1, billy, henry)
+ride5 = Ride.new(3.1, drake, franklin)
+
+# Test Methods 
+
+#Driver Tests
+harry.rides #WORKS!
+harry.passengers #WORKS!
+Driver.mileage_cap(2.3) #WORKS!
+
+#Passenger Tests
+sergio.rides  #WORKS
+sergio.drivers  #WORKS
+sergio.total_distance #WORKS
+Passenger.premium_members #WORKS
+
+#Ride Tests
+ride1.passengers  #WORKS
+ride1.drivers   #WORKS
+Ride.average_distance #WORKS
 
 
 binding.pry
